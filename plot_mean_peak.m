250344bff7f68307210472a6b6457e1d50e7bfc9
ElisabethGilbert <107705434+ElisabethGilbert@users.noreply.github.com>
2022-06-17 12:19:16 -0400

    Add files via upload

---
+++ plot_mean_peak.m
@@ -0,0 +1,25 @@
+function plot_mean_peak(folder)
+
+D = dir(['C:\MotoTrak Files\170\*.mat']);
+
+
+
+numfich = size(D,1);
+
+for i = 1:numfich
+   
+    load ('170_MotoTrak_Knob_Results_20220527_112911.mat')
+
+    
+    
+   % hit_thresh = data(:,3)
+    
+  %  figure
+  % plot(
+    
+  
+   % 
+end
+
+%file = D.name
+%data = load(strcat(D,D.name(i)));
